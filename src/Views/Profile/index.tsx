09cf3c25a2baa612ec574d41003015895f879459
Lucas Rosa <lucas.rosa@corebiz.ag>
2024-09-24 10:21:59 -0300

    tela incial profile

--- src/Views/Profile/index.tsx
+++ src/Views/Profile/index.tsx
@@ -1,14 +1,64 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from "react";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import * as S from './styles';
+import * as S from "./styles";
+import { color } from "styles/pallete";
 
 export default function Profile() {
-	return (
-		<S.container>
-			<Text>Profile</Text>
-		</S.container>
-	);
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <S.container>
+      <S.btn>
+        <Fontisto name="unlocked" size={15} color={color.interface.white} />
+        <S.textBtn>Entrar com código de acesso</S.textBtn>
+      </S.btn>
+
+      <S.inputBox>
+        <S.input
+          placeholder="Email"
+          autoFocus
+          placeholderTextColor={color.interface.darkgray}
+          keyboardType="email-address"
+        />
+      </S.inputBox>
+
+      <S.inputBox>
+        <S.input
+          secureTextEntry={showPassword}
+          placeholder="Senha"
+          placeholderTextColor={color.interface.darkgray}
+        />
+        <Ionicons
+          onPress={() => setShowPassword((value) => !value)}
+          name={showPassword ? "eye" : "eye-off"}
+          size={15}
+          color={color.interface.darkgray}
+        />
+      </S.inputBox>
+
+      <S.btn>
+        <Fontisto name="email" size={15} color={color.interface.white} />
+        <S.textBtn>Entrar com e-mail</S.textBtn>
+      </S.btn>
+
+      <S.boxforgotPassword>
+        <S.textForgotPassword color={color.interface.black}>
+          Esqueceu a senha?
+        </S.textForgotPassword>
+
+        <S.btnNoBackground>
+          <S.textForgotPassword color={color.interface.blue1}>
+            Clique aqui
+          </S.textForgotPassword>
+        </S.btnNoBackground>
+      </S.boxforgotPassword>
+
+      <S.notYetRegisterText>Ainda não é cadastrado?</S.notYetRegisterText>
+      <S.btnOutline>
+        <S.textBtnOutline>Criar nova conta</S.textBtnOutline>
+      </S.btnOutline>
+    </S.container>
+  );
 }
-
-
--- src/Views/Profile/styles.ts
+++ src/Views/Profile/styles.ts
@@ -1,7 +1,93 @@
-import styled from 'styled-components/native';
-// import { RFPercentage } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
-// import { color } from "styles/pallete";
-// import { fontsName } from "styles/fonts";
+import { color } from "styles/pallete";
+import { fontsName } from "styles/fonts";
 
-export const container = styled.View``;
+export const container = styled.View`
+  flex: 1;
+  align-items: center;
+  padding-top: 20px;
+  gap: 10px;
+`;
+
+export const btn = styled.TouchableOpacity`
+  width: 90%;
+  padding: 13px;
+  background-color: ${color.interface.blue1};
+  border-radius: 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const textBtn = styled.Text`
+  width: 90%;
+  color: ${color.interface.white};
+  font-size: ${RFPercentage(1.7)}px;
+  text-align: center;
+  font-family: ${fontsName.text};
+`;
+
+export const btnOutline = styled.TouchableOpacity`
+  width: 90%;
+  padding: 13px;
+  background-color: ${color.interface.white};
+  border: 1px solid ${color.interface.blue1};
+  border-radius: 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const textBtnOutline = styled.Text`
+  width: 90%;
+  color: ${color.interface.blue1};
+  font-size: ${RFPercentage(1.7)}px;
+  text-align: center;
+  font-family: ${fontsName.text};
+`;
+
+export const inputBox = styled.View`
+  width: 90%;
+  padding: 13px;
+  background-color: ${color.interface.lightGray2};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const input = styled.TextInput`
+  flex: 1;
+  color: ${color.interface.darkgray};
+`;
+
+export const boxforgotPassword = styled.View`
+  width: 90%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+`;
+
+export const btnNoBackground = styled.TouchableHighlight`
+  width: 100%;
+  gap: 5px;
+`;
+
+interface TextForgotPasswordProps {
+  color: string;
+}
+
+export const textForgotPassword = styled.Text<TextForgotPasswordProps>`
+  color: ${(props) => props.color};
+  font-size: ${RFPercentage(1.7)}px;
+  text-align: center;
+  font-family: ${fontsName.title};
+`;
+
+export const notYetRegisterText = styled.Text`
+  color: ${color.interface.darkgray};
+  font-size: ${RFPercentage(1.7)}px;
+  text-align: center;
+  font-family: ${fontsName.text};
+`;
