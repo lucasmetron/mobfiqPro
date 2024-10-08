b7e1d47bbc3dbe9c97d7cce28b331ed37758e8e9
Lucas Rosa <lucas.rosa@corebiz.ag>
2024-10-08 15:43:04 -0300

    ajustes feitos

--- src/Views/ProductSelected/index.tsx
+++ src/Views/ProductSelected/index.tsx
@@ -1,20 +1,32 @@
-import React, { useContext } from "react";
-import { Text } from "react-native";
+import React, { useContext, useState } from "react";
+import { Dimensions, Text } from "react-native";
 import MaterialIconsfrom from "react-native-vector-icons/MaterialIcons";
 import Fontisto from "react-native-vector-icons/Fontisto";
 import { RFPercentage } from "react-native-responsive-fontsize";
 import { useNavigation } from "@react-navigation/native";
+import Carousel from "react-native-reanimated-carousel";
 
 import * as S from "./styles";
 import { color } from "styles/pallete";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import ProductSelectedContext from "context/useProductSelected";
+import { Images, ProductProps } from "types/ProductProps";
+import { formatToBRL } from "utils/functions";
+
+interface BannerProps {
+  item: Images;
+  index: number;
+  animationValue: any;
+}
 
 export default function ProductSelected() {
+  const width = Dimensions.get("window").width;
   const navigator = useNavigation();
   const { productSelected, setProductSelected } = useContext(
     ProductSelectedContext
   );
+  const [indexPhoto, setIndexPhoto] = useState(0);
+
   console.log("productSelected: ", productSelected);
 
   return (
@@ -45,10 +57,45 @@ export default function ProductSelected() {
       </S.header>
 
       <S.content>
-        <S.carouselImgs></S.carouselImgs>
+        <S.carouselImgs>
+          <Carousel
+            loop
+            width={width}
+            autoPlay={true}
+            autoPlayInterval={5000}
+            data={productSelected?.Skus[0].Images || []}
+            scrollAnimationDuration={900}
+            vertical={false}
+            onSnapToItem={(index) => {
+              setIndexPhoto(index);
+            }}
+            renderItem={({ item }: BannerProps) => (
+              <S.item>
+                <S.image source={{ uri: item.ImageUrl }} resizeMode="stretch" />
+              </S.item>
+            )}
+          />
+          <S.containerBalls>
+            {productSelected !== null &&
+              productSelected.Skus[0].Images.map((_item: Images, i: number) => (
+                <S.ball key={i} isActive={i === indexPhoto} />
+              ))}
+          </S.containerBalls>
+        </S.carouselImgs>
+
+        <S.nameProduct numberOfLines={2}>
+          {productSelected?.Skus[0].Name || "Sem nome"}
+        </S.nameProduct>
       </S.content>
 
       <S.btnToBuy>
+        <S.line />
+        <S.price>
+          <S.priceTxt>Preço</S.priceTxt>
+          <S.priceTxt>
+            {formatToBRL(productSelected?.Skus[0].MeasurementPrice || 0)}
+          </S.priceTxt>
+        </S.price>
         <S.btnToBuyTxt>comprar</S.btnToBuyTxt>
       </S.btnToBuy>
     </S.container>
--- src/Views/ProductSelected/styles.ts
+++ src/Views/ProductSelected/styles.ts
@@ -23,6 +23,8 @@ export const header = styled.View`
 export const headerTxt = styled.Text`
   flex: 1;
   text-align: center;
+  font-family: ${fontsName.text};
+  color: ${color.interface.blue1};
 `;
 
 export const content = styled.ScrollView`
@@ -31,21 +33,85 @@ export const content = styled.ScrollView`
 `;
 
 export const carouselImgs = styled.View`
+  position: relative;
   width: 100%;
-  height: ${Dimensions.get("window").height * 0.5};
-  background-color: palegoldenrod;
+  height: ${Dimensions.get("window").height * 0.5}px;
+  background-color: ${color.interface.lightGray2};
+`;
+
+export const item = styled.View`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const image = styled.Image`
+  flex: 1;
+  width: 100%;
 `;
 
 export const btnToBuy = styled.TouchableOpacity`
   width: 100%;
-  padding: 15px;
+
   align-items: center;
   justify-content: center;
-  background-color: ${color.interface.blue1};
+`;
+
+export const price = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const line = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${color.interface.lightGray2};
+`;
+
+export const priceTxt = styled.Text`
+  font-family: ${fontsName.text};
+  font-size: ${RFPercentage(2)}px;
 `;
 
 export const btnToBuyTxt = styled.Text`
+  width: 100%;
   color: ${color.interface.white};
   font-size: ${RFPercentage(2)}px;
   text-transform: uppercase;
+  padding: 15px;
+  text-align: center;
+  background-color: ${color.interface.blue1};
+`;
+
+export const containerBalls = styled.View`
+  position: absolute;
+  bottom: 10;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 6px;
+`;
+
+interface BallProps {
+  isActive: boolean;
+}
+
+export const ball = styled.View<BallProps>`
+  width: ${RFPercentage(1)}px;
+  height: ${RFPercentage(1)}px;
+  border-radius: ${RFPercentage(0.5)}px;
+  background-color: ${color.interface.blue1};
+  opacity: ${(props) => (props.isActive ? 1 : 0.3)};
+`;
+
+export const nameProduct = styled.Text`
+  width: 100%;
+  font-size: ${RFPercentage(2.1)}px;
+  padding: 10px;
+  color: ${color.interface.darkgray};
 `;
