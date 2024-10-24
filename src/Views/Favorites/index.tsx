c2a0e28b947978855d4defe451c168c28067039f
Lucas Rosa <lucas.rosa@corebiz.ag>
2024-10-24 15:36:45 -0300

    tela quando não há favoritos

--- src/Views/Favorites/index.tsx
+++ src/Views/Favorites/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from "react";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { useNavigation } from "@react-navigation/native";
 
-import * as S from './styles';
+import * as S from "./styles";
+import { color } from "styles/pallete";
+import { stacksHome } from "Router/routes";
 
 export default function Favorites() {
-	return (
-		<S.container>
-			<Text>Favorites</Text>
-		</S.container>
-	);
+  const navigator: any = useNavigation();
+
+  return (
+    <S.container>
+      <S.noProducts>
+        <S.iconHeart>
+          <MaterialIcons
+            name="favorite"
+            color={color.interface.white}
+            size={60}
+          />
+        </S.iconHeart>
+        <S.textNoItens>
+          Para favoritar um produto clique no coração
+        </S.textNoItens>
+        <S.btnToHome
+          onPress={() => {
+            navigator.navigate(stacksHome.initial);
+          }}
+        >
+          <S.textBtn>Adicionar Produtos</S.textBtn>
+        </S.btnToHome>
+      </S.noProducts>
+    </S.container>
+  );
 }
-
-
--- src/Views/Favorites/styles.ts
+++ src/Views/Favorites/styles.ts
@@ -1,7 +1,48 @@
-import styled from 'styled-components/native';
-// import { RFPercentage } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
-// import { color } from "styles/pallete";
-// import { fontsName } from "styles/fonts";
+import { color } from "styles/pallete";
+import { fontsName } from "styles/fonts";
 
-export const container = styled.View``;
+export const container = styled.View`
+  flex: 1;
+`;
+
+export const noProducts = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  gap: 10rem;
+`;
+
+export const iconHeart = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color.interface.red};
+`;
+
+export const textNoItens = styled.Text`
+  font-size: ${RFPercentage(2.5)}px;
+  text-align: center;
+  font-family: ${fontsName.title};
+  color: ${color.interface.lightGray};
+`;
+
+export const btnToHome = styled.TouchableOpacity`
+  width: 90%;
+  border: 2px solid ${color.interface.darkgray};
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  margin-top: 20px;
+`;
+
+export const textBtn = styled.Text`
+  font-size: ${RFPercentage(2)}px;
+  font-family: ${fontsName.title};
+  color: ${color.interface.darkgray};
+`;
